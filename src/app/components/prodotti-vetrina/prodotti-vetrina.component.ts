46c399101a57cec51eb4b8a20415616dace45384
TawaGC <giucalvia99@gmail.com>
2024-06-23 11:46:48 +0200

    .

--- src/app/components/prodotti-vetrina/prodotti-vetrina.component.ts
+++ src/app/components/prodotti-vetrina/prodotti-vetrina.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProdottiService } from '../../services/prodotti.service';
+import { Prodotto } from '../../models/prodotto';
 
 @Component({
   selector: 'app-prodotti-vetrina',
   templateUrl: './prodotti-vetrina.component.html',
   styleUrl: './prodotti-vetrina.component.css'
 })
-export class ProdottiVetrinaComponent {
+export class ProdottiVetrinaComponent implements OnInit{
+  prodotti:Prodotto [] = [];
+  //Dependency Injection DI
+  constructor(private ps: ProdottiService){
+    
+  }
 
+  ngOnInit(): void {
+    this.prodotti = this.ps.getProdotti();
+    
+  }
 }
--- src/app/services/prodotti.service.ts
+++ src/app/services/prodotti.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
+import { PRODOTTI } from '../data/prodotti';
+import { Prodotto } from '../models/prodotto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdottiService {
 
-  constructor() { 
-    
+  constructor() {  }
+
+  getProdotti(){
+    return PRODOTTI as Prodotto[] ;
   }
 }
